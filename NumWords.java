7ec7c20cb0ab681504bf346dd620f0802a24c4fd
Romy Manoach <romy.manoach@post.runi.ac.il>
2025-11-05 22:10:54 +0200

    first hm fix

--- NumWords.java
+++ NumWords.java
@@ -3,7 +3,7 @@ public class NumWords {
 	public static void main(String args[]) {
 
 		int num = Integer.parseInt(args[0]);
-		int h = num % 1000 / 100;
+		int h = num / 100;
 		int t = num % 100 / 10;
 		int o = num % 10;
 
